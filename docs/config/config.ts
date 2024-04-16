44b96f6663a1b38ad2db16c358bb8f1ddbea083e
Evgeniy Shvetsov <e.shvetsov@runetsoft.ru>
2024-04-16 16:53:35 +0700

    build

---
+++ docs/config/config.ts
@@ -0,0 +1,14 @@
+export const gameConfig = {
+  startHeroesNumber: 10,
+  startEnemiesNumber: 30,
+  leftMultiValue: 2,
+  rightMultiValue: 4,
+}
+
+export const viewConfig = {
+  fontFamily: 'Marvin',
+  fontSrc: 'assets/fonts/Marvin.otf',
+  charactersDistanceX: 25,
+  charactersDistanceY: 20,
+  charactersScale: 1,
+}
